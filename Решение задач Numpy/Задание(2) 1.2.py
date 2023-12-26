adb8a62584f6ecc3e3dd7cccfb1e5978142a81c7
Derskit <148621914+Derskit@users.noreply.github.com>
2023-12-26 16:01:01 +0500

    Update Задание(2) 1.2.py

--- Решение задач Numpy/Задание(2) 1.2.py
+++ Решение задач Numpy/Задание(2) 1.2.py
@@ -3,6 +3,7 @@ n = 0
 for i in range(0,1000):
     arr = np.random.randint(1, 10, size=100)
     a = arr[arr > 7]
-    if len(a) == 20:
+    c = 100 / len(arr)
+    if len(a) * c == 20:
         n += 1
 print(n/10,'%')
